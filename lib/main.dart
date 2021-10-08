af71e66db69c01bdb8ba0af6da7c5eca3337c27b
Abiyyu Daffa' Alam <daffaalam@gmail.com>
2021-10-08 13:08:32 +0700

    Configuring the URL strategy on the web

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
+import 'common/configure_nonweb.dart'
+    if (dart.library.html) 'common/configure_web.dart';
 import 'common/custom_scroll_behavior.dart';
 import 'view/home_page.dart';
 
 void main() {
-  runApp(
-    const MainApp(),
-  );
+  configureApp(); // https://flutter.dev/docs/development/ui/navigation/url-strategies
+  runApp(const MainApp());
 }
 
 // TODO 11
@@ -18,7 +19,14 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: const HomePage(),
-      title: "Web Profile - Abiyyu Daffa Alam",
+      onUnknownRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+          builder: (BuildContext context) {
+            return const HomePage();
+          },
+        );
+      },
+      title: "Web Profile",
       theme: ThemeData.light(),
       darkTheme: ThemeData.dark(),
       scrollBehavior: CustomScrollBehavior(),
